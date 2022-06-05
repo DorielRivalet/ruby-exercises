b4d98b68cf055027ed2075569ec3f4be832083d0
DorielRivalet <100863878+DorielRivalet@users.noreply.github.com>
2022-06-05 05:28:41 -0300

    finish nested hash exercises

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -27,21 +27,27 @@ end
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
   # language_name, then return languages
-  languages[:language_name].delete_at(info_key)
+  languages[language_name].delete(info_key)
   return languages
 end
 
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
   # languages
-  languages.delete(:language_name)
+  languages.delete(language_name)
   return languages
 end
 
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
-  languages.select {|language| language[:is_beautiful?]}
+  languages1 = {}
+  languages.each do |key,value|
+    if value[:is_beautiful?] == true then
+      languages1[key] = value 
+    end
+  end 
+  languages1
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
@@ -49,7 +55,7 @@ def find_language_facts(languages, language_name, fact_index = 0)
   # key :facts and value of an array of strings) and return the fact
   # language_name has at fact_index of its facts array, or at index 0 if this
   # argument is not given
-  languages.select {|language| language[:is_beautiful?]}
+  languages.dig(language_name, :facts, fact_index)
   # the revised languages hash will look something like this:
   # {
   #   ruby: { facts: ['fact 0', 'fact 1'],
